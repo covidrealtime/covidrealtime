abd87effe29d37d63bea5af84e15d02494064c48
covidrealtime <102730872+covidrealtime@users.noreply.github.com>
2022-04-04 22:58:01 +0200

    Create Data_cleansing_functions.R

---
+++ functions/Data_cleansing_functions.R
@@ -0,0 +1,20 @@
+# Contains functions for data cleansing
+
+library(Hmisc)
+
+clean_testing <- function(test_dat)
+{
+  #Removing columns
+  test_dat <- subset(test_dat, select = c(country, country_code, testing_new_cases, tests_done, testing_population, testing_rate,
+                                          testing_positivity_rate, year, week))
+  
+  return(test_dat)
+}
+
+clean_weather <- function(weath_data)
+{
+  # Temperature is in tenths of degrees of Celsius -> calculating to Celsius
+  weath_data$tavg <- weath_data$tavg/10
+  
+  return(weath_data)
+}
