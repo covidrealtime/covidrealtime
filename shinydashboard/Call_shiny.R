a3c3ab58c932eced72f3db9fe982c13e3266eca7
covidrealtime <102730872+covidrealtime@users.noreply.github.com>
2022-04-05 22:56:23 +0200

    Create Call_shiny.R

---
+++ shinydashboard/Call_shiny.R
@@ -0,0 +1,14 @@
+# Prepares the data for the dashboard and starts the application.
+
+library(shinydashboard)
+library(shiny)
+
+
+source("helpers/Get_and_prepare_data.R")
+source("cluster/Hierarchical_cluster.R")
+source("random_forest/Random_forest.R")
+source("random_forest/Cluster_RF.R")
+source("random_forest/rank_corr.R")
+source("shinydashboard/Shiny_data_prep.R")
+
+runApp("shinydashboard")
